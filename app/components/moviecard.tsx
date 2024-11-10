a5eb9fecf36f212cd49797b02ebe1862db82ad2b
louis <louis.chopinet@epitech.eu>
2024-11-10 20:04:47 +0100

    [~] MovieCard opacity

--- app/components/moviecard.tsx
+++ app/components/moviecard.tsx
@@ -2,7 +2,7 @@ export default function MovieCard(movie: any) {
     return (
         <div className="relative transform hover:scale-105 transition-transform duration-200">
             <img src={`https://image.tmdb.org/t/p/original${movie.movie.poster_path}`} alt={movie.movie.title} className="rounded-lg" />
-            <div className="absolute inset-0 bg-black bg-opacity-75 opacity-100 lg:opacity-0 hover:opacity-100 transition-opacity duration-200 flex flex-col justify-end items-start p-4 rounded-lg">
+            <div className="absolute inset-0 bg-black bg-opacity-60 opacity-100 lg:opacity-0 hover:opacity-100 transition-opacity duration-200 flex flex-col justify-end items-start p-4 rounded-lg">
                 <h2 className="text-lg font-medium text-white mb-2">
                     {movie.movie.title}
                 </h2>
